9d5f29bafa6911db8a12ba138d12b6e6832c30dd
shera-s <sheraofficials@gmail.com>
2022-06-22 00:27:30 +0530

    commit

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
       <div id="main-content" className="ms-Grid">
         <div className="ms-Grid-row">
           <div id="title-banner" className="ms-font-su">
-            Outlook Calendar Sync Demo
+            Outlook Calendar Integration @Shera
           </div>
         </div>
         <div id="body-content" className="ms-Grid-row">
